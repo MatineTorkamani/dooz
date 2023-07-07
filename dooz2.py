60f845f83a303880f16261a3cc0c4654f3b26cef
MatineTorkamani <130989947+MatineTorkamani@users.noreply.github.com>
2023-07-07 23:06:53 +0330

    Update dooz2.py

--- dooz2.py
+++ dooz2.py
@@ -49,25 +49,3 @@ def click(game_array):
                     cat_turn = True
                     mouse_turn = False
                     game_array[i][j] = (x, y, 'mouse', False)
-
-#barande
-def has_won(game_array):
-    for row in range(len(game_array)):
-        if (game_array[row][0][2] == game_array[row][1][2] == game_array[row][2][2]) and game_array[row][0][2] != "":
-            display_message(game_array[row][0][2].upper() + " You Won! ")
-            return True
-
-    for col in range(len(game_array)):
-        if (game_array[0][col][2] == game_array[1][col][2] == game_array[2][col][2]) and game_array[0][col][2] != "":
-            display_message(game_array[0][col][2].upper() + " You Won! ")
-            return True
-
-    if (game_array[0][0][2] == game_array[1][1][2] == game_array[2][2][2]) and game_array[0][0][2] != "":
-        display_message(game_array[0][0][2].upper() + " You Won! ")
-        return True
-
-    if (game_array[0][2][2] == game_array[1][1][2] == game_array[2][0][2]) and game_array[0][2][2] != "":
-        display_message(game_array[0][2][2].upper() + " You Won! ")
-        return True
-
-    return False
