20225331ceb2cecb3d7d58da0073c5d9ad46db41
MatineTorkamani <130989947+MatineTorkamani@users.noreply.github.com>
2023-07-07 23:33:38 +0330

    update methodes for game
    like the color of screen and grid and charecters

---
+++ dooz6.py
@@ -0,0 +1,10 @@
+#update
+def render():
+    SCREEN.fill(WHITE)
+    draw_grid()
+
+    for image in images:
+        x, y, IMAGE = image
+        SCREEN.blit(IMAGE, (x - IMAGE.get_width() // 2, y - IMAGE.get_height() // 2))
+
+    pygame.display.update()
