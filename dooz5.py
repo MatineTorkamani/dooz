0f5f924abd5cc539453825a0f003ccc51c782ca4
MatineTorkamani <130989947+MatineTorkamani@users.noreply.github.com>
2023-07-08 23:48:24 +0330

    Update dooz5.py

--- dooz5.py
+++ dooz5.py
@@ -1,6 +1,6 @@
 #pm
 def display_message(content):
-    pygame.time.delay(700)
+    pg.time.delay(700)
     SCREEN.fill(WHITE)
     end_text = END_FONT.render(content, 1, BLACK)
     SCREEN.blit(end_text, ((WIDTH - end_text.get_width()) // 2, (WIDTH - end_text.get_height()) // 2))
