4818ef6c784feb4ec0950b088b53ca151129471a
MatineTorkamani <130989947+MatineTorkamani@users.noreply.github.com>
2023-07-09 16:24:18 +0330

    Update dooz1.py

--- dooz1.py
+++ dooz1.py
@@ -24,8 +24,8 @@ cat_name='data/cat.png'
 cat_img=pg.image.load(cat_name)
 cat_img=pg.transform.scale(cat_img,(150,150))
 
-mause_name='data/mause.png'
-mause_img=pg.image.load(mause_name)
-mause_img=pg.transform.scale(mause_img,(150,150))
+mouse_name='data/mause.png'
+mouse_img=pg.image.load(mouse_name)
+maouse_img=pg.transform.scale(mouse_img,(150,150))
 
 END_FONT = pg.font.SysFont('arial', 50)
